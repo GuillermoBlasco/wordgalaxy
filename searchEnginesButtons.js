3dbcd09739923fdde6176e6aaf7b59321fd3a144
lokirius <ripavifeviricoco@gmail.com>
2015-01-24 16:58:50 +0100

    Añadir twitter search y google scholar.

--- searchEnginesButtons.js
+++ searchEnginesButtons.js
@@ -8,6 +8,9 @@
             <a class=\"btn btn-default\" href=\"{{bingUrl}}\" role=\"button\" target=\"_blank\">Bing</a>\
             <a class=\"btn btn-default\" href=\"{{askUrl}}\" role=\"button\" target=\"_blank\">Ask</a>\
             <a class=\"btn btn-default\" href=\"{{duckduckGo}}\" role=\"button\" target=\"_blank\">DuckDuckGo</a>\
+            <a class=\"btn btn-default\" href=\"{{twitterUrl}}\" role=\"button\" target=\"_blank\">Twitter</a>\
+            <a class=\"btn btn-default\" href=\"{{scholarUrl}}\" role=\"button\" target=\"_blank\">Scholar</a>\
+
         </div>\
         ";
     var service = function(topic, panel) {
@@ -17,12 +20,17 @@
             bing : encodeURIComponent(topic.replace(" ", "+")),
             ask : encodeURIComponent(topic.replace(" ", "+")),
             duckduckgo : encodeURIComponent(topic.replace(" ", "+"))
+            twitter : encodeURIComponent(topic)
+            scholar : encodeURIComponent(topic.replace(" ", "+"))
         };
         var urlData = {
             googleUrl : Mustache.render("https://www.google.es/webhp#q={{google}}", data),
             bingUrl : Mustache.render("http://www.bing.com/search?q={{bing}}", data),
             askUrl : Mustache.render("http://www.ask.com/web?q={{ask}}", data),
             duckduckGo : Mustache.render("https://duckduckgo.com/?q={{duckduckgo}}", data)
+            twitter : Mustache.render("https://twitter.com/search?q={{twitter}} filter%3Alinks", data)
+            scholarUrl : Mustache.render("https://scholar.google.es/scholar?q={{scholar}}", data),
+            
         };
 
         panel.prepend(Mustache.render(template, urlData));

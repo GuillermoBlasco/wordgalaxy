9c32ca6a3aee670f9c4a07940c328c279d251efa
GuillermoBlasco <guillermo.blasco.jimenez@gmail.com>
2015-01-24 18:53:57 +0100

    added dbpedia links

--- js/refServices/dbpediaService.js
+++ js/refServices/dbpediaService.js
@@ -13,9 +13,11 @@
             success : function(data) {
                 console.log("RAW: " + data);
                 data = JSON.parse(data);
+                panel=$("body > div.container-fluid.tb-wrapper > div.preview > div:nth-child(3) > ul:nth-child(4)");
                 for (var i = 0; i < data.length; i++) {
-                    panel.append("<a href=\"" + data[i].name + "\">" + data[i].name + "</a>");
+                    panel.append("<li><a href=\"" + data[i].name + "\">" + data[i].name + "</a></li>");
                 }
+                $("body > div.container-fluid.tb-wrapper > div.preview > div:nth-child(3) > h5:nth-child(3) > span").text("(" + data.length + ")");
             }
         });
 
